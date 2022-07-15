20cc3cfe39ad456dac383625f2ddee44a9681c02
Benjamin Ng <ng.kk.benjamin@gmail.com>
2022-07-15 11:27:19 +0800

    Added some usv parameters, SITL connection to GCS changed to UDP
    Alvintps reference commit id: 2c22843

--- Tools/autotest/swarminit.sh
+++ Tools/autotest/swarminit.sh
@@ -1,21 +1,18 @@
 !/bin/bash
 
 # Variable declaration
-address='127.0.0.1'
 location='PandanReservoir'
 AP_PATH='/home/ra-lwj-apollo/ardupilot/Rover'
-GCS_PATH='/home/ra-lwj-apollo/Documents/'
+QGC_PATH='/home/ra-lwj-apollo/Documents/QGroundControl.AppImage'
 count=2
 
 echo "Initializing multiple USV instances in SITL..."
 for (( i=1; i<=$count; i++ ))
 do
     echo "Starting Vehicle $i..."
-    (( port=$i+8000 ))
-    gnome-terminal --title="Vehicle $i" --tab -- bash -c "cd $AP_PATH;sim_vehicle.py -L $location -f usv --sysid $i -I $i --out=tcpin:$address:$port -Z swarminit.txt"
+    gnome-terminal --title="Vehicle $i" --tab -- bash -c "cd $AP_PATH;sim_vehicle.py -L $location -v Rover -f usv --sysid $i -I $i -Z swarminit.txt"
     sleep 0.1
 done
 
 echo "Starting QGroundControl..."
-cd $GCS_PATH
-./QGroundControl.AppImage
+${QGC_PATH}
