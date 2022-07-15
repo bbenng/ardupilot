cbedfa1ff2bd2a4d9f102eff781f14e2b3366e7a
Benjamin Ng <ng.kk.benjamin@gmail.com>
2022-07-15 10:51:10 +0800

    Added multiple vehicle support for USV
    Added 3 new files: \n /Rover/mav_2_1.parm \n /Tools/autotest/default_params/usv-1.parm and usv-2.parm \n And edited /Tools/autotest/default_params/pysim/vehicleinfo.py

--- Tools/autotest/pysim/vehicleinfo.py
+++ Tools/autotest/pysim/vehicleinfo.py
@@ -370,6 +370,16 @@ class VehicleInfo(object):
                 "default_params_filename": ["default_params/rover.parm",
                                             "default_params/usv.parm"],
             },
+                    "usv-1": {
+                "waf_target": "bin/ardurover",
+                "default_params_filename": ["default_params/rover.parm",
+                                            "default_params/usv-1.parm"],
+            },
+            "usv-2": {
+                "waf_target": "bin/ardurover",
+                "default_params_filename": ["default_params/rover.parm",
+                                            "default_params/usv-2.parm"],
+            },
         },
     },
     "ArduSub": {
