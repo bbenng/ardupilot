ec8aecbb90931cebe58478e66c48adbda3f0e216
Benjamin Ng <ng.kk.benjamin@gmail.com>
2022-07-15 10:18:57 +0800

    Adding 'USV' SITL frame type
    Added 2 new files, SIM_USV.cpp and SIM_USV.h into /libraries/SITL.

---
+++ libraries/SITL/SIM_USV.cpp
@@ -0,0 +1,215 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+    usv simulator class
+    see explanation of lift and drag explained here: https://en.wikipedia.org/wiki/Forces_on_sails
+    To-Do: - add heel handling by calculating lateral force from wind vs gravity force from heel to arrive at roll rate or acceleration
+           - use thrust curve to calculate total thrust
+*/
+
+#include "SIM_USV.h"
+#include <AP_Math/AP_Math.h>
+#include <AP_AHRS/AP_AHRS.h>
+#include <string.h>
+#include <stdio.h>
+
+extern const AP_HAL::HAL& hal;
+
+namespace SITL {
+
+// very roughly sort of a stability factors for waves
+#define WAVE_ANGLE_GAIN 1
+#define WAVE_HEAVE_GAIN 1
+
+USV::USV(const char *frame_str) :
+    Aircraft(frame_str),
+    turning_circle(1.8),
+    turn_rate(140.0)
+{}
+
+// return yaw rate in deg/sec given a steering input (in the range -1 to +1) and speed in m/s
+float USV::get_yaw_rate(float steering, float speed) const
+{
+    if (is_zero(steering) || is_zero(speed)) {
+        return 0;
+    }
+    return steering * turn_rate;
+}
+
+// return lateral acceleration in m/s/s given a steering input (in the range -1 to +1) and speed in m/s
+float USV::get_lat_accel(float steering, float speed) const
+{
+    float yaw_rate = get_yaw_rate(steering, speed);
+    float accel = radians(yaw_rate) * speed;
+    return accel;
+}
+
+// simulate basic waves / swell
+void USV::update_wave(float delta_time)
+{
+    const float wave_heading = sitl->wave.direction;
+    const float wave_speed = sitl->wave.speed;
+    const float wave_lenght = sitl->wave.length;
+    const float wave_amp = sitl->wave.amp;
+
+    // apply rate propositional to error between boat angle and water angle
+    // this gives a 'stability' effect
+    float r, p, y;
+    dcm.to_euler(&r, &p, &y); 
+
+    // if not armed don't do waves, to allow gyro init
+    if (sitl->wave.enable == 0 || !hal.util->get_soft_armed() || is_zero(wave_amp) ) { 
+        wave_gyro = Vector3f(-r,-p,0.0f) * WAVE_ANGLE_GAIN;
+        wave_heave = -velocity_ef.z * WAVE_HEAVE_GAIN;
+        wave_phase = 0.0f;
+        return;
+    }
+
+    // calculate the sailboat speed in the direction of the wave
+    const float boat_speed = velocity_ef.x * sinf(radians(wave_heading)) + velocity_ef.y * cosf(radians(wave_heading));
+
+    // update the wave phase
+    const float aprarent_wave_distance = (wave_speed - boat_speed) * delta_time;
+    const float apparent_wave_phase_change = (aprarent_wave_distance / wave_lenght) * M_2PI;
+
+    wave_phase += apparent_wave_phase_change;
+    wave_phase = wrap_2PI(wave_phase);
+
+    // calculate the angles at this phase on the wave
+    // use basic sine wave, dy/dx of sine = cosine
+    // atan( cosine ) = wave angle
+    const float wave_slope = (wave_amp * 0.5f) * (M_2PI / wave_lenght) * cosf(wave_phase);
+    const float wave_angle = atanf(wave_slope);
+
+    // convert wave angle to vehicle frame
+    const float heading_dif = wave_heading - y;
+    float angle_error_x = (sinf(heading_dif) * wave_angle) - r;
+    float angle_error_y = (cosf(heading_dif) * wave_angle) - p;
+
+    // apply gain
+    wave_gyro.x = angle_error_x * WAVE_ANGLE_GAIN;
+    wave_gyro.y = angle_error_y * WAVE_ANGLE_GAIN;
+    wave_gyro.z = 0.0f;
+
+    // calculate wave height (NED)
+    if (sitl->wave.enable == 2) {
+        wave_heave = (wave_slope - velocity_ef.z) * WAVE_HEAVE_GAIN;
+    } else {
+        wave_heave = 0.0f;
+    }
+}
+
+/*
+  update the usv simulation by one time step
+ */
+void USV::update(const struct sitl_input &input)
+{
+    float steering, throttle;
+
+    // update wind
+    update_wind(input);
+
+    // the usv uses skid steering mode, i.e. the steering and throttle values are used for motor1 and motor2
+    float motor1 = 2*((input.servos[0]-1000)/1000.0f - 0.5f);
+    float motor2 = 2*((input.servos[2]-1000)/1000.0f - 0.5f);
+    steering = motor1 - motor2;
+    throttle = 0.5*(motor1 + motor2);
+
+    // calculate apparent wind in earth-frame (this is the direction the wind is coming from)
+    // Note than the SITL wind direction is defined as the direction the wind is travelling to
+    // This is accounted for in these calculations
+    Vector3f wind_apparent_ef = wind_ef + velocity_ef;
+    const float wind_apparent_speed = safe_sqrt(sq(wind_apparent_ef.x)+sq(wind_apparent_ef.y));
+
+    // set RPM and airspeed from wind speed, allows to test RPM and Airspeed wind vane back end in SITL
+    rpm[0] = wind_apparent_speed;
+    airspeed_pitot = wind_apparent_speed;
+
+    // how much time has passed?
+    float delta_time = frame_time_us * 1.0e-6f;
+
+    // speed in m/s in body frame
+    Vector3f velocity_body = dcm.transposed() * velocity_ef_water;
+
+    // speed along x axis, +ve is forward
+    float speed = velocity_body.x;
+
+    // yaw rate in degrees/s
+    float yaw_rate = get_yaw_rate(steering, speed);
+
+    gyro = Vector3f(0,0,radians(yaw_rate)) + wave_gyro;
+
+    // update attitude
+    dcm.rotate(gyro * delta_time);
+    dcm.normalize();
+
+    // throttle force
+    float throttle_force = 2.0f * throttle * thrust_curve[20] * GRAVITY_MSS;
+
+    // hull drag (R_T = 0.5 * p * S * V^2 * C_T)
+    // gives hull drag == throttle force at 3m/s
+    float hull_drag = sq(speed) * 0.5f * 22.89f;
+    if (!is_positive(speed)) {
+        hull_drag *= -1.0f;
+    }
+
+    // accel in body frame due acceleration from sail and deceleration from hull friction
+    accel_body = Vector3f(throttle_force - hull_drag, 0, 0);
+    accel_body /= mass;
+
+    // add in accel due to direction change
+    accel_body.y += radians(yaw_rate) * speed;
+
+    // now in earth frame
+    // remove roll and pitch effects from waves
+    float r, p, y;
+    dcm.to_euler(&r, &p, &y);
+    Matrix3f temp_dcm;
+    temp_dcm.from_euler(0.0f, 0.0f, y);
+    Vector3f accel_earth = temp_dcm * accel_body;
+
+    // we are on the ground, so our vertical accel is zero
+    accel_earth.z = 0 + wave_heave;
+
+    // work out acceleration as seen by the accelerometers. It sees the kinematic
+    // acceleration (ie. real movement), plus gravity
+    accel_body = dcm.transposed() * (accel_earth + Vector3f(0, 0, -GRAVITY_MSS));
+
+    // tide calcs
+    Vector3f tide_velocity_ef;
+     if (hal.util->get_soft_armed() && !is_zero(sitl->tide.speed) ) {
+        tide_velocity_ef.x = -cosf(radians(sitl->tide.direction)) * sitl->tide.speed;
+        tide_velocity_ef.y = -sinf(radians(sitl->tide.direction)) * sitl->tide.speed;
+        tide_velocity_ef.z = 0.0f;
+     }
+
+    // new velocity vector
+    velocity_ef_water += accel_earth * delta_time;
+    velocity_ef = velocity_ef_water + tide_velocity_ef;
+
+    // new position vector
+    position += velocity_ef * delta_time;
+
+    // update lat/lon/altitude
+    update_position();
+    time_advance();
+
+    // update magnetic field
+    update_mag_field_bf();
+
+    // update wave calculations
+    update_wave(delta_time);
+
+}
+
+} // namespace SITL
---
+++ libraries/SITL/SIM_USV.h
@@ -0,0 +1,69 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+  usv simulator class
+*/
+
+#pragma once
+
+#include "SIM_Aircraft.h"
+
+namespace SITL {
+
+/*
+  a usv simulator
+ */
+class USV : public Aircraft {
+public:
+    USV(const char *frame_str);
+
+    /* update model by one time step */
+    void update(const struct sitl_input &input) override;
+
+    /* static object creator */
+    static Aircraft *create(const char *frame_str) {
+        return new USV(frame_str);
+    }
+
+    bool on_ground() const override {return true;};
+
+private:
+
+    // return turning circle (diameter) in meters for steering angle proportion in the range -1 to +1
+    float get_turn_circle(float steering) const;
+
+    // return yaw rate in deg/sec given a steering input (in the range -1 to +1) and speed in m/s
+    float get_yaw_rate(float steering, float speed) const;
+
+    // return lateral acceleration in m/s/s given a steering input (in the range -1 to +1) and speed in m/s
+    float get_lat_accel(float steering, float speed) const;
+
+    // simulate waves and swell
+    void update_wave(float delta_time);
+
+    float turning_circle;       // vehicle minimum turning circle diameter in meters
+    float turn_rate;            // vehicle maximum turn rate in degrees/second
+
+    // thrust curve based on Blue Robotics T200 thruster data at 16V
+    // PWM values                     1100   1140   1180   1220   1260   1300   1340   1380   1420   1460  1500  1540  1580  1620  1660  1700  1740  1780  1820  1860  1900
+    const float thrust_curve[21] = {-4.07f,-3.66f,-2.99f,-2.45f,-1.94f,-1.44f,-1.02f,-0.65f,-0.32f,-0.07f,0.00f,0.08f,0.40f,0.82f,1.28f,1.82f,2.43f,3.11f,3.82f,4.65f,5.25f}; // kgf
+
+    const float mass = 6.0f;
+
+    Vector3f velocity_ef_water; // m/s
+    Vector3f wave_gyro;         // rad/s
+    float wave_heave;           // m/s/s
+    float wave_phase;           // rads
+};
+
+} // namespace SITL
