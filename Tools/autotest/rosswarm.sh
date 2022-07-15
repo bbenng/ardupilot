16d18f81bb5c1ff00840fa1d0e4b794c14461dbe
Benjamin Ng <ng.kk.benjamin@gmail.com>
2022-07-15 11:35:43 +0800

    Edited rosswarm to follow ROS C++ naming conventions
    Alvintps reference commit id: 452f266

--- Tools/autotest/rosswarm.sh
+++ Tools/autotest/rosswarm.sh
@@ -3,7 +3,7 @@
 # Usage: ./rosswarm.sh [count]
 # Note: Ensure that the offboard ROS package has been built successfully.
 # https://github.com/alvintps/offboard
-# Offboard ROS package: https://github.com/alvintps/offboard
+# APM offboard mode ROS package: https://github.com/alvintps/apm_offboard_mode
 
 # Variable declaration
 count="$1"
@@ -12,6 +12,6 @@ echo "Initializing $count USV instances in ROS..."
 for (( i=1; i<=$count; i++ ))
 do
     echo "Starting Vehicle $i..."
-    gnome-terminal --title="Vehicle $i" --tab -- bash -c "roslaunch offboard apm_single_usv.launch tgt_system:=$i"
+    gnome-terminal --title="Vehicle $i" --tab -- bash -c "roslaunch apm_offboard_mode apm_single_usv.launch tgt_system:=$i"
     sleep 1
 done
